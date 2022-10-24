2330f66dc445e5548dd385e6c1f588ac26f02d73
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2022-10-24 09:31:03 -0700

    first attempt

---
+++ untitled0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 08:49:37 2022
+
+@author: student
+"""
+import numpy as np
+A = [2.2, 3.5, 6.7]
+print(len(A))
+
+print(np.zeros(3))
